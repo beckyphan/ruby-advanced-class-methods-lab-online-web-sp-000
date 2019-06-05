65f0587ac8b95356912b595a1895582bc2f512fc
Becky Phan <beckyphan@live.com>
2019-06-05 22:46:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   
   def self.find_by_name(name)
     @@all.each do |song|
-      song.detect {song.name == name}
+      song.detect {|song| song.name == name}
         return song
       end 
     end
